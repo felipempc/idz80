fc23af8d7baf8825302dcbc06665cb778c706122
Felipe Correia <idz80a@gmail.com>
2026-04-12 21:06:02 -0300

    codeview_evmouse.cpp: reviewed.

--- src/codeview_evmouse.cpp
+++ src/codeview_evmouse.cpp
@@ -17,29 +17,29 @@
 
 
 
+/// @brief Processes the left button mouse clicked down event
+/// @param event 
 void CodeView::onMouseLeftDown(wxMouseEvent& event)
 {
 	int lastposition;
 	wxPoint pt;
 
-    if (!IsEnabled())
-    {
+    if (!IsEnabled()) {
         return;
     }
 
     SetFocus();
-    if (!m_multi_selection)
+    if (!m_multi_selection) {
         m_line_info.cursorLastPosition = m_line_info.cursorPosition;
+    }
 
 	lastposition = m_line_info.cursorPosition;
 	pt = event.GetPosition();
     calcCursorPosition(pt);
-	if (m_line_info.cursorPosition < getCount())
-	{
+	if (m_line_info.cursorPosition < getCount()) {
 		clearCursor();
 
-		if (m_line_info.cursorPosition > getLastLine())
-		{
+		if (m_line_info.cursorPosition > getLastLine()) {
 			Scroll(-1, getFirstLine() + 1);
 			RefreshRect(calcCursorRfshRect());
 		}
@@ -50,48 +50,54 @@ void CodeView::onMouseLeftDown(wxMouseEvent& event)
 
 		RefreshRect(calcCursorRfshRect());
 	}
-	else
+	else {
 		m_line_info.cursorPosition = lastposition;
+    }
 
 }
 
 
+
+/// @brief Processes the left button mouse clicked up event
+/// @param event 
 void CodeView::onMouseLeftUp(wxMouseEvent& event)
 {
     fillSelectedItemInfo(event.GetPosition());
 
-    if (HasCapture())
+    if (HasCapture()) {
         ReleaseMouse();
+    }
 
 }
 
 
 
+/// @brief  Processes the right button mouse clicked down event
+/// @param event 
 void CodeView::onMouseRightDown(wxMouseEvent& event)
 {
 	int	lastposition;
 	wxPoint pt;
 
-    if (!IsEnabled())
-    {
+    if (!IsEnabled()) {
         return;
     }
 
     SetFocus();
 
-    if (!m_multi_selection)
+    if (!m_multi_selection) {
         m_line_info.cursorLastPosition = m_line_info.cursorPosition;
+    }
 
 	lastposition = m_line_info.cursorPosition;
 	pt = event.GetPosition();
     calcCursorPosition(pt);
-	if (m_line_info.cursorPosition < getCount())
-	{
+	if (m_line_info.cursorPosition < getCount()) {
 		clearCursor();
 
 		if (m_multi_selection && ((m_line_info.cursorPosition < m_line_info.firstLine) ||
-			(m_line_info.cursorPosition > m_line_info.lastLine)))
-		{
+			(m_line_info.cursorPosition > m_line_info.lastLine))) {
+
 			RefreshRect(calcSelectedRect());
 			m_line_info.selectedLineCount = 1;
 			m_multi_selection = false;
@@ -99,39 +105,37 @@ void CodeView::onMouseRightDown(wxMouseEvent& event)
 			m_line_info.cursorLastPosition = m_line_info.cursorPosition;
 			m_line_info.lastLine = m_line_info.cursorPosition;
 		}
-		else
-			if (!m_multi_selection)
-			{
+		else {
+			if (!m_multi_selection) {
 				m_line_info.selectedLineCount = 1;
 				m_line_info.firstLine = m_line_info.cursorPosition;
 				m_line_info.cursorLastPosition = m_line_info.cursorPosition;
 				m_line_info.lastLine = m_line_info.firstLine;
 			}
-
+        }
 		fillSelectedItemInfo(pt);
-
 		RefreshRect(calcCursorRfshRect());
 	}
-	else
+	else {
 		m_line_info.cursorPosition = lastposition;
-
+    }
 }
 
 
+
+/// @brief Processes the right button mouse clicked up event
+/// @param event 
 void CodeView::onMouseRightUp(wxMouseEvent& event)
 {
     wxMenu      *PopUp;
 
-    if (!m_is_empty)
-    {
+    if (!m_is_empty) {
         PopUp = new wxMenu();
 
-        if (m_line_info.selectedLineCount > 1)
-        {
+        if (m_line_info.selectedLineCount > 1) {
             createPopupMenuMultiSelection(PopUp);
         }
-        else
-        {
+        else {
             createPopupMenuSingleSelection(PopUp);
         }
 
@@ -142,40 +146,43 @@ void CodeView::onMouseRightUp(wxMouseEvent& event)
 
 
 
-
-
 // TODO: Rewrite this
+
+/// @brief Processes the mouse move
+/// @param event 
 void CodeView::onMouseMove(wxMouseEvent& event)
 {
-	if (event.Dragging())
-	{
-	    if (!m_selecting)
+	if (event.Dragging()) {
+	    if (!m_selecting) {
             m_line_info.cursorLastPosition = m_line_info.cursorPosition;
+        }
 
-        if (!HasCapture())
+        if (!HasCapture()) {
             CaptureMouse();
+        }
 
 	    calcCursorPosition(event.GetPosition());
         clearCursor();
 
-		if (m_incomplete_area.Contains(event.GetPosition()) && (m_line_info.cursorPosition < getLastItemIndex()))
-		{
+		if (m_incomplete_area.Contains(event.GetPosition()) && (m_line_info.cursorPosition < getLastItemIndex())) {
 			Scroll(-1, getFirstLine() + 1);
 			RefreshRect(calcCursorRfshRect());
 		}
 
-	    if (m_selecting)
-        {
+	    if (m_selecting) {
             updateSelectedRect();
             RefreshRect(calcSelectedRect());
         }
-        else
+        else {
             RefreshRect(calcCursorRfshRect());
+        }
 	}
 }
 
 
 
+/// @brief Processes the mouse wheel scrolling event.
+/// @param event 
 void CodeView::onMouseWheel(wxMouseEvent& event)
 {
     bool up_motion;
@@ -184,28 +191,36 @@ void CodeView::onMouseWheel(wxMouseEvent& event)
 
     up_motion = (event.GetWheelRotation() >= 0);
 
-    if (up_motion)
-    {
+    if (up_motion) {
         position = getFirstLine() - motion_step;
-        if (position < 0)
+        if (position < 0) {
             position = 0;
+        }
         Scroll(-1,position);
     }
-    else
-    {
+    else {
         position = getFirstLine() + motion_step;
-        if (position > static_cast<int>(m_sourcecode->getCount() - m_lines_shown))
+        if (position > static_cast<int>(m_sourcecode->getCount() - m_lines_shown)) {
             position = m_sourcecode->getCount() - m_lines_shown;
+        }
         Scroll(-1,position);
     }
 }
 
+
+
+/// @brief Processes the entering in window mouse event
+/// @param event 
 void CodeView::onMouseEnterWindow(wxMouseEvent& event)
 {
     LogIt("Mouse enter the CodeView window...");
 }
 
 
+
+
+/// @brief Processes the leaving window mouse event
+/// @param event 
 void CodeView::onMouseLeaveWindow(wxMouseEvent& event)
 {
     LogIt("...Mouse leave the CodeView window.");
